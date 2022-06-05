26ffa48185f6acff0736ded3e4d27b29398ae553
Marcelo <marcelo.dev100@gmail.com>
2022-06-05 17:06:07 -0300

    Uso do token JWT com Swagger

--- ExoApi/Program.cs
+++ ExoApi/Program.cs
@@ -58,8 +58,32 @@ builder.Services.AddSwaggerGen(doc => {
         { 
             Name = "Aluno: Marcelo Marques." ,
             Url = new Uri("https://github.com/MarceloDev100")
-        }    
+        }
    });
+
+    doc.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme(){
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme.\r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\""
+    });
+
+    doc.AddSecurityRequirement(new OpenApiSecurityRequirement{
+         {
+             new OpenApiSecurityScheme
+             {
+                 Reference = new OpenApiReference
+                 {
+                     Type = ReferenceType.SecurityScheme,
+                     Id = "Bearer"
+                 }
+             },
+             new string[]{}
+         }
+    }) ;
+
      var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
      var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
      doc.IncludeXmlComments(xmlPath);
